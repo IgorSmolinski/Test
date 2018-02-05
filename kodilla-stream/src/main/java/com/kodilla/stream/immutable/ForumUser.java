6925cf0f97e79ba2e676d78b9be4ffb0f6aa7b34
IgorSmolinski <pooky7@gmail.com>
2018-02-06 00:46:41 +0100

    7.2

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -0,0 +1,18 @@
+gpackage com.kodilla.stream.immutable;
+
+public final class ForumUser {
+    private final String username, realname;
+
+    public ForumUser(final String username, final String realname) {
+        this.username = username;
+        this.realname = realname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+}
